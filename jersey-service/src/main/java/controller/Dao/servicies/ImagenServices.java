3c005b9e1e1d4df8b0b0e26bf2e868650825cf22
Sebastian <joseph.narvaez@unl.edu.ec>
2024-12-12 20:43:54 -0500

    Union con receta
    Se le asigna una receta a una imagen

--- jersey-service/src/main/java/controller/Dao/servicies/ImagenServices.java
+++ jersey-service/src/main/java/controller/Dao/servicies/ImagenServices.java
@@ -3,26 +3,48 @@ package controller.Dao.servicies;
 import controller.Dao.ImagenDao;
 import controller.tda.list.LinkedList;
 import models.Imagen;
+import models.Receta;
+import java.util.HashMap;
 
 public class ImagenServices {
+
+    public Object[] listShowall() throws Exception {
+        if (!obj.getlistAll().isEmpty()) {
+            Imagen[] lista = (Imagen[]) obj.getlistAll().toArray();
+            Object[] respuesta = new Object[lista.length];
+            for (int i = 0; i < lista.length; i++) {
+                Receta p = new RecetaServicies().get(lista[i].getIdReceta());
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("id", lista[i].getIdImagen());
+                map.put("url", lista[i].getIdImagen());
+                map.put("receta", p);
+                respuesta[i] = map;
+            }
+            return respuesta;
+        }
+        return new Object[] {};
+    }
+
     private ImagenDao obj;
-    public ImagenServices(){
+
+    public ImagenServices() {
         obj = new ImagenDao();
     }
-    public Boolean save() throws Exception{
+
+    public Boolean save() throws Exception {
         return obj.save();
     }
 
-    public LinkedList<Imagen> listAll(){     
+    public LinkedList<Imagen> listAll() {
         return obj.getlistAll();
 
     }
-    
-    public Imagen getImagen(){
+
+    public Imagen getImagen() {
         return obj.getImagen();
     }
 
-    public void setImagen( Imagen imagen){
+    public void setImagen(Imagen imagen) {
         obj.setImagen(imagen);
     }
 }
